320c195b2f717cf3de91e4771b0cddded7647bc8
Paulo Carvalho <phcarvalhodev@gmail.com>
2020-08-13 23:52:42 -0300

    Updates

---
+++ precache-manifest.73f028cfcd742d923f6b4661f306d711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54f8bf82bacae61ef9206940806e696",
+    "url": "/gitrepos-tracker/index.html"
+  },
+  {
+    "revision": "8902837b8d1485310d18",
+    "url": "/gitrepos-tracker/static/css/main.5bdd99f4.chunk.css"
+  },
+  {
+    "revision": "00478d8f75859bfc8534",
+    "url": "/gitrepos-tracker/static/js/2.e42366f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gitrepos-tracker/static/js/2.e42366f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8902837b8d1485310d18",
+    "url": "/gitrepos-tracker/static/js/main.cef508d9.chunk.js"
+  },
+  {
+    "revision": "12e8d24f18f83c2c7bae",
+    "url": "/gitrepos-tracker/static/js/runtime-main.eda31f48.js"
+  }
+]);
